0c013cf1fc22168168acbdcb93bb267a1b3142f2
HelenaOliveira1 <helenna54319@gmail.com>
2016-10-10 15:14:45 -0300

    CaixaCores

---
+++ Cor.py
@@ -0,0 +1,9 @@
+cor = []
+
+def AdicionarCor():
+        cor.append(input("Digite a cor: "))
+
+def ListarCores(cor):
+    for i in cor:
+        print(i)
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import Cor
+
+
+def main():
+    print("Opção 1 - Adicionar Cor , Opção 2 - Listar Cor , Opção 3 - Encerrar Programa")
+    op = int(input("Qual opção deseja usar? "))
+
+    while (op != 3):
+        if (op == 1):
+            Cor.AdicionarCor()
+            print("Opção 1 - Adicionar Cor , Opção 2 - Listar Cor , Opção 3 - Encerrar Programa")
+            op = int(input("Qual opção deseja usar? "))
+
+        elif (op == 2):
+            Cor.ListarCores(Cor.cor)
+            print("Opção 1 - Adicionar Cor , Opção 2 - Listar Cor , Opção 3 - Encerrar Programa")
+            op = int(input("Qual opção deseja usar? "))
+
+    if (op == 3):
+        print("Programa Encerrado.")
+
+main()
