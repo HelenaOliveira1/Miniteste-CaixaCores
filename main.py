a7dd07c4a9a597b2b25b9b7d21f0d304a2e9a12b
HelenaOliveira1 <helenna54319@gmail.com>
2016-10-10 15:16:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ def main():
     while (op != 3):
         if (op == 1):
             Cor.AdicionarCor()
+            print("")
             print("Opção 1 - Adicionar Cor , Opção 2 - Listar Cor , Opção 3 - Encerrar Programa")
             op = int(input("Qual opção deseja usar? "))
 
         elif (op == 2):
             Cor.ListarCores(Cor.cor)
+            print("")
             print("Opção 1 - Adicionar Cor , Opção 2 - Listar Cor , Opção 3 - Encerrar Programa")
             op = int(input("Qual opção deseja usar? "))
 
     if (op == 3):
+        print("")
         print("Programa Encerrado.")
 
 main()
